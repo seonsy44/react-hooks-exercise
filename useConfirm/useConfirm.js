88db1cf8b7ad22bd2200ac07d0e88ec614e3a0c1
seon <seonsy44@gmail.com>
2022-03-30 17:51:51 +0900

    useConfirm 수정

--- useConfirm/useConfirm.js
+++ useConfirm/useConfirm.js
@@ -1,9 +1,18 @@
 export const useConfirm = (message = "Are you sure?", onConfirm, onCancel) => {
-  if (!onConfirm || typeof onConfirm !== "function") return;
-  if (!onCancel || typeof onCancel !== "function") return;
+  if (typeof onConfirm !== "function") return;
+  if (typeof onCancel !== "function") return;
 
   const confirmAction = () => {
     confirm(message) ? onConfirm() : onCancel();
+    if (confirm(message)) {
+      onConfirm();
+    } else {
+      try {
+        onCancel();
+      } catch (error) {
+        return;
+      }
+    }
   };
 
   return confirmAction;
